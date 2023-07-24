1c1d68cdf5ca308e7907be0757debd92f4328855
keple <keplerde@gmail.com>
2023-07-24 17:43:11 +0200

    Scss styles and fonts

--- components/shared/Header.js
+++ components/shared/Header.js
@@ -1,31 +1,28 @@
 import React from 'react';
 import Link from 'next/link';
 
-
-
-
 class Header extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <p className='customClass'> I am styled P element </p>
-        <p className='customClassFromFile'> I am styled P element </p>
-        <Link style={{'fontSize': '50px'}} href="/"> Home  </Link>{' '} 
-        <Link href="/about">About  </Link>
-        <Link href="/portfolios">Portfolio   </Link>
-        <Link href="/blogs">Blog  </Link>
+        <div className='parent-class'>
+        <h1>Test Scss </h1> {/* Corrected closing h1 tag */}
+        </div>
+        
+        <Link style={{'fontSize': '50px'}} href="/"> Home </Link>{' '}
+        <Link href="/about">About</Link>
+        <Link href="/portfolios">Portfolio</Link>
+        <Link href="/blogs">Blog</Link>
         <Link href="/cv">CV</Link>
         <style jsx>
-         {
-          `
+          {`
           a {
             font-size: 20px;
-          };
+          }
           .customClass {
             color: red;
           }
-          `
-         }
+          `}
         </style>
       </React.Fragment>
     );
