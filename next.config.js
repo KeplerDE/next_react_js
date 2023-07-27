fb09a3e86212d150ff99365958cdff1fb16ff879
keple <keplerde@gmail.com>
2023-07-27 15:51:23 +0200

    absolute_path

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
-const path = require('path')
- 
+const path = require('path');
+
 module.exports = {
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-  },
+  webpack: config => {
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
+  }
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import "../styles/main.scss";
+import "@/styles/main.scss";
 
 
 export default function MyApp({ Component, pageProps }) {
