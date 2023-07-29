435b38bc0d260d1437d071c4b11e4c07efa2a692
keple <keplerde@gmail.com>
2023-07-29 08:55:47 +0200

    fetching_by_id

--- pages/portfolios/index.js
+++ pages/portfolios/index.js
@@ -1,28 +1,14 @@
-import { useEffect, useState } from 'react';
-import BaseLayout from "../../components/layouts/BaseLayout";
-import BasePage from "../../components/BasePage";
-import Link from "next/link";
-import axios from "axios";
+import BaseLayout from '@/components/layouts/BaseLayout';
+import BasePage from '@/components/BasePage';
+import Link from 'next/link';
+import { useGetData } from '@/actions';
 
-
-
-  const Portfolios = () => {
-    const [posts, setPosts] = useState([]);
-  
-    useEffect(() => {
-      async function getPosts() {
-        const res = await fetch('/api/v1/posts');
-        const data = await res.json();
-        setPosts(data);
-      }
-  
-      getPosts();
-    }, [])
-
-  const renderPosts = () => {
+const Portfolios = () => {
+  const { data, error, loading } = useGetData('api/v1/posts');
+  const renderPosts = (posts) => {
     return posts.map(post =>
-      <li key={post.id}>
-        <Link href={`/portfolios/${post.id}`}>
+      <li key={post.id} style={{'fontSize': '20px'}}>
+        <Link as={`/portfolios/${post.id}`} href="/portfolios/[id]">
           <a>
             {post.title}
           </a>
@@ -34,25 +20,21 @@ import axios from "axios";
   return (
     <BaseLayout>
       <BasePage>
-        <h1>I am portfolios page</h1>
-        <ul>
-          {renderPosts()}
-        </ul>
+        <h1>I am Portfolio Page</h1>
+        { loading &&
+          <p>Loading data...</p>
+        }
+        { data &&
+          <ul>
+            {renderPosts(data)}
+          </ul>
+        }
+        { error &&
+          <div className="alert alert-danger">{error.message}</div>
+        }
       </BasePage>
-    </BaseLayout >
+    </BaseLayout>
   )
-}
-
-Portfolios.getInitialProps = async () => {
-  let posts = [];
-  try {
-    const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
-    posts = res.data;
-  } catch(e) {
-    console.error(e);
-  }
-
-  return { posts: posts.slice(0, 10) };
 }
 
 export default Portfolios;
