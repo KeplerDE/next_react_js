17239d4166cd78f30f6f9f5801a957337a90e874
keple <keplerde@gmail.com>
2023-08-03 19:09:52 +0200

    env variables

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import "@/styles/main.scss";
 
 
-export default function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }) {s
   return <Component {...pageProps} />
 }
