7e8cf8f96a5dd331d2b13cff2d14621bd1fb9d81
keple <keplerde@gmail.com>
2023-08-16 07:52:55 +0200

    mongo.db

--- portfolio-api/index.js
+++ portfolio-api/index.js
@@ -2,6 +2,33 @@
 
 const express = require('express');
 const server = express();
+const config = require('./config/dev');
+
+// Импортируем mongoose
+const mongoose = require('mongoose'); 
+
+// Функция для подключения к базе данных 
+async function connectToDb() {
+
+  // Используем async/await
+  try {
+    
+    // Подключаемся и передаем параметры  
+    await mongoose.connect(config.DB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    
+    console.log('Connected to DB!');
+    
+  } catch (error) {
+    console.log('Connection error', error);
+  }
+
+}
+
+// Вызываем функцию подключения
+connectToDb();
 
 server.use('/api/v1/portfolios', require('./routes/portfolios'));
 
