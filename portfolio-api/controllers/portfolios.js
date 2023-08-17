de0242afd082f3ce5371c8e79b259ae2c1711ff4
keple <keplerde@gmail.com>
2023-08-18 01:59:11 +0200

    get_portf_by_id_js

--- portfolio-api/controllers/portfolios.js
+++ portfolio-api/controllers/portfolios.js
@@ -12,3 +12,23 @@ exports.getPortfolios = async (req, res) => {
   return res.json(portfolios); 
 
 }
+
+exports.getPortfolioById = async (req, res) => {
+
+  try {
+
+    // Ищем портфолио по ID из параметров запроса
+    const portfolio = await Portfolio.findById(req.params.id);
+    
+    // Возвращаем найденный документ в JSON 
+    return res.json(portfolio);
+
+  } catch(error) {
+
+    // Обрабатываем ошибку поиска
+    // Возвращаем статус 422 и текст ошибки
+    return res.status(422).send(error.message);
+
+  }
+
+}
