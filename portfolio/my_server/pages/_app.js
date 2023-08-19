6c8312d1d5263e94277acd7db1e6da4dcbc9439e
keple <keplerde@gmail.com>
2023-08-19 18:21:23 +0200

    date

--- portfolio/my_server/pages/_app.js
+++ portfolio/my_server/pages/_app.js
@@ -1,5 +1,8 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "@/styles/main.scss";
+import 'react-datepicker/dist/react-datepicker.css';
+
+
 
 
 export default function MyApp({ Component, pageProps }) {
