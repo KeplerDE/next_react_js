f93884adcea1778fcf7d69f88de0af80f8dd5c52
keple <keplerde@gmail.com>
2023-09-07 06:06:37 +0200

    mutate_function

--- portfolio/my_server/pages/dashboard.js
+++ portfolio/my_server/pages/dashboard.js
@@ -1,20 +1,21 @@
 import BaseLayout from '@/components/layouts/BaseLayout';
 import BasePage from '@/components/BasePage';
-import { withAuth } from 'utils/auth0';
+import withAuth from 'hoc/withAuth';
 import { Row, Col } from 'reactstrap';
 import Masthead from 'components/shared/Masthead';
 import PortDropdown from 'components/shared/Dropdown';
 import Link from 'next/link';
-import { useUpdateBlog } from 'actions/blogs';
+import { useUpdateBlog, useGetUserBlogs } from 'actions/blogs';
+import { toast } from 'react-toastify';
 
-import auth0 from 'utils/auth0';
-import BlogApi from 'lib/api/blogs';
-
-const Dashboard = ({user, blogs}) => {
+const Dashboard = ({user, loading}) => {
   const [updateBlog] = useUpdateBlog();
+  const {data: blogs, mutate} = useGetUserBlogs();
 
   const changeBlogStatus = async (blogId, status) => {
-    await updateBlog(blogId, {status});
+    updateBlog(blogId, {status})
+      .then(() => mutate())
+      .catch(() => toast.error('Something went wrong...'));
   }
 
   const createOption = (blogStatus) => {
@@ -27,17 +28,17 @@ const Dashboard = ({user, blogs}) => {
 
     return [
       { key: `${blog._id}-published`,
-      text: option.view,
-      handlers: {
-        onClick: () => changeBlogStatus(blog._id, option.value)}
-    },
+        text: option.view,
+        handlers: {
+          onClick: () => changeBlogStatus(blog._id, option.value)}
+      },
       {key: `${blog._id}-delete`,text: 'Delete', handlers: { onClick: () => {alert(`Clicking Delete! ${blog._id}`)}}}
     ]
   }
 
   const renderBlogs = (blogs, status) => (
     <ul className="user-blogs-list">
-      { blogs.filter(blog => blog.status === status).map(blog =>
+      { blogs && blogs.filter(blog => blog.status === status).map(blog =>
         <li key={blog._id}>
           <Link href="/blogs/editor/[id]" as={`/blogs/editor/${blog._id}`}>
             <a>{blog.title}</a>
@@ -50,7 +51,7 @@ const Dashboard = ({user, blogs}) => {
   )
 
   return (
-    <BaseLayout navClass="transparent" user={user} loading={false}>
+    <BaseLayout navClass="transparent" user={user} loading={loading}>
       <Masthead imagePath="/images/home-bg.jpg" />
       <BasePage className="blog-user-page">
         <Row>
@@ -68,9 +69,4 @@ const Dashboard = ({user, blogs}) => {
   )
 }
 
-export const getServerSideProps = withAuth(async ({req, res}) => {
-  const { accessToken } = await auth0.getSession(req);
-  const json = await new BlogApi(accessToken).getByUser();
-  return { blogs: json.data }
-})('admin');
-export default Dashboard;
+export default withAuth(Dashboard)('admin');
