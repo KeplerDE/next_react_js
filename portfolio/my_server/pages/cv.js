9ee5b020b53c988513e4d417cabe9028591120fb
keple <keplerde@gmail.com>
2023-09-09 20:33:42 +0200

    cv_page

--- portfolio/my_server/pages/cv.js
+++ portfolio/my_server/pages/cv.js
@@ -1,15 +1,20 @@
-import BaseLayout from "@/components/layouts/BaseLayout";
-import BasePage from "@/components/BasePage";
+import BaseLayout from '@/components/layouts/BaseLayout';
+import BasePage from '@/components/BasePage';
 import { useGetUser } from '@/actions/user';
+import { Row, Col } from 'reactstrap';
 
 const Cv = () => {
   const { data, loading } = useGetUser();
   return (
-    <BaseLayout  user={data} loading={loading}>
+    <BaseLayout user={data} loading={loading}>
       <BasePage>
-        <h1>I am cv page</h1>
+        <Row>
+          <Col md={{size: 8, offset: 2}}>
+            <iframe style={{width: '100%', height: '800px'}} src="/Denis_Osipov_Junior_Full_Stack.pdf"/>
+          </Col>
+        </Row>
       </BasePage>
-    </BaseLayout >
+    </BaseLayout>
   )
 }
 
