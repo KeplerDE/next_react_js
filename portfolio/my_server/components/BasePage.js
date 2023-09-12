866c7a96016c9333355a5d071d0fd6f01842999b
keple <keplerde@gmail.com>
2023-09-13 01:30:20 +0200

    portfolio_detail

--- portfolio/my_server/components/BasePage.js
+++ portfolio/my_server/components/BasePage.js
@@ -2,9 +2,17 @@ import { Container } from 'reactstrap';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 
+
+const PageHeader = ({header}) =>
+  <div className="page-header">
+    <h1 className="page-header-title">{header}</h1>
+  </div>
+
+
 const BasePage = props => {
   const router = useRouter();
   const {
+    noWrapper,
     indexPage,
     className = '',
     header,
@@ -27,19 +35,25 @@ const BasePage = props => {
         <meta property="og:type" key="og:type" content="website" />
         <meta property="og:description" key="og:description" content={metaDescription} />
         <meta property="og:image" key="og:image" content={`${process.env.BASE_URL}/images/section-3.jpg`} />
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;700&display=swap" rel="stylesheet"></link>
+        <link rel="icon" type="image/x-icon" href="/images/favicon-32x32.png"/>
         <link
           rel="canonical"
           href={`${process.env.BASE_URL}${canonicalPath ? canonicalPath : router.asPath}`} />
       </Head>
       <div className={`${pageType} ${className}`}>
-        <Container>
-          { header &&
-            <div className="page-header">
-              <h1 className="page-header-title">{header}</h1>
-            </div>
-          }
-          {children}
-        </Container>
+      { noWrapper &&
+          <>
+            { header && <PageHeader header={header} /> }
+            {children}
+          </>
+        }
+        { !noWrapper &&
+          <Container>
+            { header && <PageHeader header={header} /> }
+            {children}
+          </Container>
+        }
       </div>
     </>
   )
